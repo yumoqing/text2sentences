17c0e06e11ce6dd77304042e9da1a2ede91edd87
yumoqing <yumoqing@gmail.com>
2022-08-14 22:40:49 -0400

    bugfix

--- text2sentences/detectlang.py
+++ text2sentences/detectlang.py
@@ -2,10 +2,8 @@ from langdetect import detect_langs
 
 def detect_lang(x):
 	y = detect_langs(x)
-	print(y)
 	s = None
 	for m in y:
-		print(m, type(m),dir(m))
 		if s == None:
 			s = {
 				'lang':m.lang,
